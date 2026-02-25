5c653219e67538671d81990e8b53473b0d1fe09a
DAIWEI <david810213@gmail.com>
2026-02-25 20:55:52 +0800

    first commit

--- src/ParkingSystem.java
+++ src/ParkingSystem.java
@@ -1,5 +1,6 @@
 public class ParkingSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to the Online Parking System with Pricing Module");
+        System.out.println("Please select a parking lot:");
     }
 }
