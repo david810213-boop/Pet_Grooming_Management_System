2136e74073241fa0e05af1a855cf36909c9686a6
DAIWEI <david810213@gmail.com>
2026-02-25 20:56:46 +0800

    Second commit

--- src/ParkingSystem.java
+++ src/ParkingSystem.java
@@ -2,5 +2,7 @@ public class ParkingSystem {
     public static void main(String[] args) {
         System.out.println("Welcome to the Online Parking System with Pricing Module");
         System.out.println("Please select a parking lot:");
+        System.out.println("1. Lot A (10 spaces)");
+        System.out.println("2. Lot B (20 spaces)");
     }
 }
